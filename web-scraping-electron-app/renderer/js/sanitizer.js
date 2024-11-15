79ebd54767b5f61580d6cfd5e4517a8c5d9d56b4
dgallenb <122754827+dgallenb@users.noreply.github.com>
2024-11-15 16:12:14 -0700

    Added HTML tag remover

--- web-scraping-electron-app/renderer/js/sanitizer.js
+++ web-scraping-electron-app/renderer/js/sanitizer.js
@@ -8,6 +8,8 @@
  * 
  * ex.
  * var s = new Sanitizer('<li class="%toggle %"\'ESCAPE^>', new SanitizeProtocol(), {"ESCAPE": "escape", "toggle": "TOGGLE"});
+ * 
+ * If you just want the presets already built in, call [sanitizerObject].htmlMode() or [sanitizerObject].sqlMode().
  * */
 class Sanitizer {
 	constructor(input, sanitizeProtocol, expressionMap = {}) {
@@ -44,6 +46,14 @@ class Sanitizer {
 		this.expressionMap = expressionMap;
 	}
 
+	addToExpressionMap(key, value) {
+		this.expressionMap[key] = value;
+	}
+
+	/**
+	 * Settings to strip &, <, >, ", ', “, ”, ‘, ’
+	 * Replaces each of the characters with their HTML hexcodes.
+	 * */
 	htmlMode() {
 		this.sanitizeProtocol = new SanitizeProtocol("[&<>\"'/“”‘’]", 
 			{
@@ -58,9 +68,13 @@ class Sanitizer {
 		      "‘": '&#x27;',
 		      "’": '&#x27;',
 			});
-		expressionMap = {};
+		this.expressionMap = {};
 	}
 
+	/**
+	 * Settings to strip %, _, ^, ESCAPE, ', [, ], /
+	 * Replaces ^ with the HTML hexcode for ↑, _ with -, [] with (), ESCAPE with escape
+	 * */
 	sqlMode() {
 		const sqlMap = {
 	      '%': '&#x25;',
@@ -71,21 +85,29 @@ class Sanitizer {
 	      "[": '(',
 	      "]": ')',
 		};
-		this.sanitizeProtocol = new SanitizeProtocol("[%_^\'\/\[\]]", sqlMap);
+		this.sanitizeProtocol = new SanitizeProtocol("[%_^\'\/\\[\\]]", sqlMap);
 		this.expressionMap = {'ESCAPE': 'escape'};
 	}
 
-	sanitize() {
-		var regexTransformed = this.sanitizeProtocol.sanitize(this.input);
+	/**
+	 * Sanitizes the data following the rules established by the sanitizer's creation.
+	 * @param input 		The text to sanitize. If none is given, the object will use whatever text was input via the constructor initially.
+	 * @return String 		The sanitized text.
+	 */
+	sanitize(input = "") {
+		if(input == "") {
+			input = this.input;
+		}
+		var regexTransformed = this.sanitizeProtocol.sanitize(input);
 		/*
 		// Unused, as the expressionMap is now an object literal, not a Map object.
 		const iterator = this.expressionMap.keys();
 		for(var i = 0; i < this.expressionMap.size; ++i) {
 			var key = iterator.next().value;
-			console.log("Key: " + key + ", value: " + this.expressionMap.get(key));
 
 			regexTransformed = regexTransformed.replaceAll(key,this.expressionMap.get(key));
 		}*/
+
 		for(var key in this.expressionMap) {
 			regexTransformed = regexTransformed.replaceAll(key, this.expressionMap[key]);
 		}
@@ -94,44 +116,61 @@ class Sanitizer {
 	}
 
 	/**
-	 * Strips %, _, ^, ESCAPE, ', [, ], /
-	 * Replaces ^ with the HTML hexcode for ↑, _ with -, [] with (), ESCAPE with escape
-	 * */
-	sanitizeSQL() {
-		const sqlMap = {
-	      '%': '&#x25;',
-	      '_': '-',
-	      '^': '&#x2191;',
-	      "'": '&#x27;',
-	      "/": '&#x2F;',
-	      "[": '(',
-	      "]": ')',
-		};
-		var sqlProtocol = new SanitizeProtocol("[%_^\'\/]", sqlMap);
+	 * Removes HTML tags from the input text. Can also sanitize the data.
+	 * @param input 		The text to sanitize. If none is given, the object will use whatever text was input via the constructor initially.
+	 * @param alsoSanitize	Whether to also run the sanitize() method on the output. Defaults to true.
+	 * @return String 		The sanitized text.
+	 */
+	removeTags(input = "", alsoSanitize = true) {
+		if(input == "") {
+			input = this.input;
+		}
 
-		var firstPass = sqlProtocol.sanitize(this.input);
-		return firstPass.replaceAll('ESCAPE', 'escape');
-	}
+		var min = 0;
+		var transformedInput = "";
+		var substringInput = input;
 
+		var keepGoing = true;
+		while(keepGoing) {
+			if(substringInput.indexOf('<') < min) {
+				keepGoing = false;
+			}
+			else {
+				// Check for whether the < is part of a tag.
+				// HTML tags cannot have a space after the opening <, 
+				// so the current test is to check for a space after the <, 
+				// and whether there's a closing > after the <. 
+				if(substringInput.charAt(substringInput.indexOf('<') + 1) === " ") {
+					
+					transformedInput += substringInput.slice(min, substringInput.indexOf('<') + 1);
+					substringInput = substringInput.slice(substringInput.indexOf('<') + 1);
+				}
+				else if(substringInput.indexOf('>') >= min) {
+					var start = substringInput.indexOf('<');
+					var end = substringInput.indexOf('>') + 1;
+					
+					transformedInput += substringInput.slice(min, start);
+					substringInput = substringInput.slice(end);
+				}
+				else {
+					keepGoing = false;
+				}
+				
+			}
+		}
+		if(alsoSanitize) {
+			var regexTransformed = this.sanitizeProtocol.sanitize(input);
 
-	/**
-	 * Strips certain characters from the text, replacing them with the usual safe HTML indicators.
-	 * */
-	basicHTMLSanitize() {
-		const map = {
-	      '&': '&amp;',
-	      '<': '&lt;',
-	      '>': '&gt;',
-	      '"': '&quot;',
-	      "'": '&#x27;',
-	      "/": '&#x2F;',
-	      "“": '&quot;',
-	      "”": '&quot;',
-	      "‘": '&#x27;',
-	      "’": '&#x27;',
-		};
-		const reg = /[&<>"'/“”‘’]/ig;
-		return this.input.replace(reg, (match)=>(map[match]));
+			for(var key in this.expressionMap) {
+				regexTransformed = regexTransformed.replaceAll(key, this.expressionMap[key]);
+			}
+
+			return regexTransformed;		
+		}
+		else {
+			return transformedInput;
+		}
+		
 	}
 }
 
@@ -139,20 +178,7 @@ class Sanitizer {
 // Note: this is a second class. I would normally move this to another file, but since it's only used as part of this script, it simplifies the imports needed.
 class SanitizeProtocol {
 
-	constructor(regexString = "[&<>\"'/“”‘’]", 
-		sanitizationMapping = {
-	      '&': '&amp;',
-	      '<': '&lt;',
-	      '>': '&gt;',
-	      '"': '&quot;',
-	      "'": '&#x27;',
-	      "/": '&#x2F;',
-	      "“": '&quot;',
-	      "”": '&quot;',
-	      "‘": '&#x27;',
-	      "’": '&#x27;',
-		}
-	) {
+	constructor(regexString = "", sanitizationMapping = {}) {
 		this.regexString = regexString;
 		this.sanitizationMapping = sanitizationMapping;
 
@@ -169,6 +195,10 @@ class SanitizeProtocol {
 //const t = new Sanitizer('<li class="%toggle %"\'ESCAPE^>', new SanitizeProtocol(), new Map([["ESCAPE", "escape"], ["toggle", "TOGGLE"]]));
 
 //t.addToMap("ESCAPE", "escape");
-const t = new Sanitizer('<li class="%toggle()\]\[ %"\'ESCAPE^>', new SanitizeProtocol(), {"ESCAPE": "escape", "toggle": "TOGGLE"});
-t.sqlMode();
-console.log(t.sanitize());
+/*
+const t = new Sanitizer('<script src="../node_modules/bootstrap/dist/js/bootstrap.bundle.js">Test ><>text</script>', new SanitizeProtocol(), {"ESCAPE": "escape", "toggle": "TOGGLE"});
+t.htmlMode();
+
+t.addToExpressionMap("script", "REPLACED");
+console.log(t.removeTags("", false));
+*/
