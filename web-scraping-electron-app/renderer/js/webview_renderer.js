5897616ecce3765a35095e698b8ed403a9f73c39
dgallenb <122754827+dgallenb@users.noreply.github.com>
2024-11-23 18:28:17 -0700

    Not catching the event yet for some reason
    Need to switch gears. Will finish this tomorrow if I need to.

--- web-scraping-electron-app/renderer/js/webview_renderer.js
+++ web-scraping-electron-app/renderer/js/webview_renderer.js
@@ -1,5 +1,5 @@
 const ipcRenderer = window.urlScrape;
-/*
+
 function createRightClickMenu() {
     
     var menu = document.createElement('div');
@@ -14,6 +14,8 @@ function createRightClickMenu() {
     menu.style.listStyleType = "none";
     menu.style.listStyle = "none";
     menu.style.cursor = "pointer";
+    menu.style.left = "100px";
+    menu.style.top = "100px";
 
     var textNode = document.createTextNode("Import");
     menu.appendChild(textNode);
@@ -40,7 +42,6 @@ function hideMenu() {
     menu.style.display = "none";
 }
 
-*/
 
 document.addEventListener('DOMContentLoaded', () => {
     
@@ -58,5 +59,34 @@ document.addEventListener('DOMContentLoaded', () => {
 
     webview.addEventListener('ipc-message', function(event, selection) {
         $('#imported_textarea').val(event.args[0]);
+    });
+
+    webview.addEventListener('contextmenu', function(event) {
+        alert("test");
+        // Prevent the default context menu from appearing
+        //  event.preventDefault();
+        webview.console.log("Test");
+        showMenu(event);
+        console.log("click");
+    });
+
+
+    webview.addEventListener('click', function(event) {
+        alert("test");
+        webview.console.log("click");
+        console.log("click");
+        
+        var menu = document.getElementById("importedRightClickMenu");
+        // check if the click was in the expected region of the menu.
+        const menuDims = menu.getBoundingClientRect();
+        if((event.pageX >= menuDims.left) && (event.pageX <= menuDims.right) &&
+            (event.pageY >= menuDims.top) && (event.pageY <= menuDims.bottom)) {
+            importText();
+
+        }
+        else {
+            hideMenu();
+        }
+
     });
 });
