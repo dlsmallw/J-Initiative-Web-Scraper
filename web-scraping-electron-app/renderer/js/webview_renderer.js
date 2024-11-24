da371eb7ac9984b58ddbae8e888cad7a48a2ed72
dgallenb <122754827+dgallenb@users.noreply.github.com>
2024-11-24 14:16:31 -0700

    Method used here isn't a good solution

--- web-scraping-electron-app/renderer/js/webview_renderer.js
+++ web-scraping-electron-app/renderer/js/webview_renderer.js
@@ -8,7 +8,7 @@ function createRightClickMenu() {
     menu.id = "importedRightClickMenu";
     menu.style.zIndex = 1000;
     menu.style.position = "absolute";
-    menu.style.display = "block";
+    menu.style.display = "none";
     menu.style.border = "1px solid black";
     menu.style.borderRadius = "4px";
     menu.style.padding = "2px";
@@ -23,20 +23,23 @@ function createRightClickMenu() {
     menu.appendChild(textNode);
 
     document.body.appendChild(menu);
-
 }
 
 function createTopLayer() {
     var layer = document.createElement('div');
-    layer.id = "topLayer";
+    layer.id = "topmostLayer";
     layer.style.zIndex = 999;
     layer.style.position = "absolute";
     layer.style.display = "block";
-    layer.style.minWidth = window.innerWidth + "px";
-    layer.style.minHeight = window.innerHeight + "px";
-    //layer.style.backgroundColor = "#ff0000";
+    layer.style.width = window.innerWidth + "px";
+    layer.style.height = window.innerHeight + "px";
 
-    layer.addEventListener('click', catchClick(event));
+    layer.style.backgroundColor = "#ff0000";
+    layer.style.opacity = 0.2;
+
+    document.addEventListener('click', function(event) {
+        catchClick(event);
+    });
     layer.addEventListener('contextmenu', function(event) {
         catchRightClick(event);
     });
@@ -50,18 +53,27 @@ function catchRightClick(event) {
 
 function catchClick(event) {
     //alert("catch click: " + event.srcElement);
+    //console.log("caught a click. Menu visible: " + menuVisible);
     var menu = document.getElementById("importedRightClickMenu");
     // check if the click was in the expected region of the menu.
     const menuDims = menu.getBoundingClientRect();
-    if((event.pageX >= menuDims.left) && (event.pageX <= menuDims.right) &&
+
+    if(menuVisible) {
+        //console.log("Menu is visible");
+        if((event.pageX >= menuDims.left) && (event.pageX <= menuDims.right) &&
         (event.pageY >= menuDims.top) && (event.pageY <= menuDims.bottom)) {
-        importText();
-        console.log("imported");
+            importText();
+            console.log("imported");
+        }
+        else {
+            hideMenu();
+            console.log("hidden");
+        }
     }
     else {
-        hideMenu();
-        console.log("hidden");
+        getItemAtClick(event);
     }
+    
 }
 
 function importText() {
@@ -80,9 +92,62 @@ function showMenu(event) {
 
 function hideMenu() {
     var menu = document.getElementById("importedRightClickMenu");
-
+    menu.style.display = "none";
     menuVisible = false;
-    //menu.style.display = "none";
+    
+}
+
+function hideFrame() {
+    const layer = document.getElementById("topmostLayer");
+    if(layer === null) {
+
+    }
+    else {
+        layer.style.display = "none";
+        layer.width = "0px";
+        layer.height = "0px";
+    }
+    
+}
+
+function unhideFrame() {
+    const layer = document.getElementById("topmostLayer");
+    if(layer === null) {
+
+    }
+    else {   
+        layer.style.display = "block";
+        layer.style.width = window.innerWidth + "px";
+        layer.style.height = window.innerHeight + "px";
+    }
+}
+
+function getItemAtClick(event) {
+    let yPos = event.pageY;
+    let xPos = event.pageX;
+    console.log("Positions: " + xPos + ", " + yPos);
+    hideFrame();
+
+    if((xPos > window.innerWidth) || (xPos < 0) || (yPos > window.innerHeight) || (yPos < 0)) {
+        // do nothing, because the click is outside boundaries
+    }
+    else {
+        if(!isFinite(xPos) || !isFinite(yPos)) {
+
+        }
+        else {
+            let elem = document.elementFromPoint(xPos,yPos);
+            console.log(Object.getOwnPropertyNames(elem));
+            let text = elem.textContent;
+
+            $('#imported_textarea').val(text);
+            console.log("grabbed: " + text);    
+        }
+        
+    }
+    //unhideFrame();
+
+    
 }
 
 
@@ -111,17 +176,17 @@ document.addEventListener('DOMContentLoaded', () => {
     $('#importSelectedBtn').on('click', () => {
         webview.send('getSelected');
 
-        document.getElementById('ext-url-window').addEventListener('click', catchClick(event));
-        alert("add");
+        //document.getElementById('ext-url-window').addEventListener('click', catchClick(event));
+        //alert("add");
     });
 
     webview.addEventListener('ipc-message', function(event, selection) {
         $('#imported_textarea').val(event.args[0]);
         //document.getElementById('ext-url-window').addEventListener('click', catchClick(event));
-        alert("added");
+        //alert("added");
     });
 
-    document.addEventListener('click', () => {alert("click")});
+    //document.addEventListener('click', () => {alert("click")});
 
     /*
     document.getElementById('ext-url-window').contentWindow.addEventListener('contextmenu', function(event) {
@@ -135,7 +200,7 @@ document.addEventListener('DOMContentLoaded', () => {
     */
 
     //document.addEventListener('click', catchClick(event));
-
+    /*
     document.getElementById('ext-url-window').contentWindow.addEventListener('click', function(event) {
         //alert("test");
         //console.log(document.getElementById('ext-url-window').contentWindow.constructor.name);
@@ -147,5 +212,5 @@ document.addEventListener('DOMContentLoaded', () => {
         
         //catchClick(event);
 
-    });
+    });*/
 });
