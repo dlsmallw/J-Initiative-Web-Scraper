d5b0a0dcb8c9e3bce83a5cdb86aadb3ed989adf6
dgallenb <122754827+dgallenb@users.noreply.github.com>
2024-12-01 19:21:29 -0700

    Should be better practice while still erroring appropriately

--- web-scraping-electron-app/tests/test.js
+++ web-scraping-electron-app/tests/test.js
@@ -1,6 +1,9 @@
+const assert = require('assert');
 
-test();
-console.assert(1 == 2, "Testing 1 == 2");
+runTests();
+
+
+
 
 function test() {
 	if(1 == 1) {
@@ -12,3 +15,20 @@ function test() {
 	}
 }
 
+
+function runTests() {
+	try {
+		assert(1 == 2, "Testing 1 == 2");
+	}
+	catch(err) {
+		if(err instanceof assert.AssertionError) {
+			console.log("testing failure");
+			process.exitCode = 1;
+		}
+		else {
+			// Some other error occurred?
+			console.log("Unexpected error!");
+			process.exitCode = 1;
+		}
+	}
+}
