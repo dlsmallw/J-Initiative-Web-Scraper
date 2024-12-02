5763d24fc08994a28f5ce72ab334754ab31c44da
dgallenb <122754827+dgallenb@users.noreply.github.com>
2024-12-01 19:06:26 -0700

    Made erroneous operations. Running this file should fail

--- web-scraping-electron-app/tests/test.js
+++ web-scraping-electron-app/tests/test.js
@@ -1,3 +1,13 @@
 
 
-console.assert(1 == 2, "testing 1 = 2");
+console.assert(1 == 2, test());
+
+function test() {
+	if(1 == 2) {
+		let a = 1 + {'1'};
+		throw new Error('Error forced');
+		
+	}
+}
+
+test();
