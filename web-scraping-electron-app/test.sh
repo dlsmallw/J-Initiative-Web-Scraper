13eb622a1a3c0acd9471901bc8f9f4d50ae6a95c
dgallenb <122754827+dgallenb@users.noreply.github.com>
2025-02-08 11:31:30 -0700

    function isn't working yet
    trying to execute arbitrary commands to cut out versions of each file

--- web-scraping-electron-app/test.sh
+++ web-scraping-electron-app/test.sh
@@ -11,11 +11,22 @@ findVersion(){
 
 testFunc(){
 	baseCall=eval $1
-	toFind=$2
-	pos=`expr index "$baseCall" ${toFind}`
 
-	return $pos
+	toFind=$2
+	toEnd=$3
+	pos=`expr index "$baseCall" ${toFind}`
+	pos1=$(($pos + ${#toFind} - 1))
+	pos2=`expr index "${baseCall:$pos1}" $toEnd`
+	result="${baseCall:${pos1}:${pos2}}"
+	
+	echo $pos1
+	return $result
 	
 }
 
-t=$(testFunc "pip --version" "pip")
+val="pip --version"
+echo ${val:0:2}
+
+testFunc "pip --version" "pip "
+
+vs=$(testFunc "pip --version" "pip " " ")
