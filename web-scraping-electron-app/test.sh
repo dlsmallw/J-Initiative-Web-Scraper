68e31c080d6703d570b6a5eb8110241e791756ba
dgallenb <122754827+dgallenb@users.noreply.github.com>
2025-02-07 18:12:43 -0700

    Trying to convert script into a function call

--- web-scraping-electron-app/test.sh
+++ web-scraping-electron-app/test.sh
@@ -1,8 +1,21 @@
-basePipCall="$(pip --version)"
-toFindPip='pip'
-pos=`expr index "$basePipCall" ${toFindPip}`
-newStr=${basePipCall:${pos}}
-pos1=$(($pos + ${#toFindPip}))
-pos2=`expr index "${basePipCall:$pos1}" ' '`
-pipVersion="${basePipCall:${pos1}:${pos2}}"
-echo "${pipVersion}"
+findVersion(){
+	baseCall=eval $1
+	toFind=$2
+	pos=`expr index "$baseCall" ${toFind}`
+	newStr=${baseCall:${pos}}
+	pos1=$(($pos + ${#toFind}))
+	pos2=`expr index "${baseCall:$pos1}" ' '`
+	
+	return "${baseCall:${pos1}:${pos2}}"
+}
+
+testFunc(){
+	baseCall=eval $1
+	toFind=$2
+	pos=`expr index "$baseCall" ${toFind}`
+
+	return $pos
+	
+}
+
+t=$(testFunc "pip --version" "pip")
