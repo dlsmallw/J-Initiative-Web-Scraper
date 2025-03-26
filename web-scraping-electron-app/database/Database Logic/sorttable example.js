07a2c67dc438b01d94af56ddb1ab1706d9ce2362
nawhitak <nawhitak@asu.edu>
2025-03-26 23:57:05 +0700

    Task-#328 implement an example implementation of sorttable

---
+++ web-scraping-electron-app/database/Database Logic/sorttable example.js
@@ -0,0 +1,20 @@
+<script src="sorttable.js"></script>
+
+<table class="sortable">
+  <tr>
+    <th>Website</th>
+    <th>Entries</th>
+    <th>Last Accessed</th>
+  </tr>
+
+  <tr class="item">
+    <td>Site 1</td>
+    <td>Entry 1</td>
+    <td>Date 1</td>
+  </tr>
+  <tr class="item">
+    <td>Site 2</td>
+    <td>Entry 2</td>
+    <td>Date 2</td>
+  </tr>
+</table>
